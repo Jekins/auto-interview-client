7ee818bb6b646a4e7f8c119d6199fc6deb1685e3
Artem Zhekov <jekinsd@gmail.com>
2019-07-19 19:38:59 +0300

    Start sign up page

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import ViewSignUp from './views/ViewSignUp.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'view-sign-up',
+      component: ViewSignUp
     },
     {
       path: '/about',
@@ -22,4 +22,4 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
-})
+});
---
+++ src/views/ViewSignUp.vue
@@ -0,0 +1,37 @@
+<script>
+  // import HelloWorld from '@/components/HelloWorld.vue'
+
+  export default {
+    name: 'view-sign-up',
+
+    components: {
+    }
+  }
+</script>
+
+<template>
+  <div class="view-sign-up">
+    <main class="view-sign-up__content">
+      <h1 class="view-sign-up__title">
+        Автоматическое тестирование
+      </h1>
+
+      <div class="view-sign-up__lead">
+        <p>
+          Это автоматическое тестирование по знаниям JavaScript, HTML и CSS.
+          <br>
+          На решения <b>всех заданий</b> выделено <b>20 минут</b>.
+          Вы сами выбираете сколько времени потратить на решение той или иной задачи.
+          Как только время иссякнет, ваш ответ будет автоматически отправлен и тестированые закончится.
+        </p>
+        <p>
+          <b>Желаем удачи!</b>
+        </p>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+  @import '../styles/views/view-sign-up';
+</style>
